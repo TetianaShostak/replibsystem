de0f093982f05547c6594fa71f75b9be8e90b808
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-07 20:08:06 +0200

    Librarian.cpp added

---
+++ Librarian.cpp
@@ -0,0 +1,30 @@
+//
+// Created by tanya on 07.03.2024.
+//
+
+#include "Librarian.h"
+#include <iostream>
+using namespace std;
+
+Librarian::Librarian(const string& _name, int _age)
+        : name(_name), age(_age) {
+    cout << "Librarian constructor called." << endl;
+}
+
+Librarian::Librarian() : Librarian("Unknown", 0) {}
+
+Librarian::~Librarian() {
+    cout << "Librarian destructor called." << endl;
+}
+
+void Librarian::getName() const {
+    cout << "Librarian Name: " << name << endl;
+}
+
+void Librarian::getAge() const {
+    cout << "Librarian Age: " << age << endl;
+}
+void Librarian::addBookToLibrary(Book& book) {
+}
+void Librarian::removeBookFromLibrary(Book& book) {
+}
