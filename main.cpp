e3eda93ba9d8c2f02e50351c67063676c13f6bcc
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-07 20:14:21 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "Librarian.h"
+#include "Book.h"
+#include "User.h"
+using namespace std;
+
+int main() {
+    Book book1("1984", "George Orwell", 1949);
+    User user1("Mary Dave", 18);
+    Librarian librarian1("Jane Sun", 19);
+
+    book1.getTitle();
+    book1.getAuthor();
+    book1.getYearPublished();
+
+    user1.getName();
+    user1.getAge();
+
+    librarian1.getName();
+    librarian1.getAge();
+
+    return 0;
+}
