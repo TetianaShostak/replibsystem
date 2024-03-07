adc555e5fbc8b570a30c3cd3e2e32b6fd1464a3d
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-07 20:10:49 +0200

    User.cpp added

---
+++ User.cpp
@@ -0,0 +1,31 @@
+//
+// Created by tanya on 07.03.2024.
+//
+
+#include "User.h"
+#include <iostream>
+using namespace std;
+
+User::User(const string& _name, int _age)
+        : name(_name), age(_age) {
+    cout << "User constructor called." << endl;
+}
+
+User::User() : User("Unknown", 0) {}
+//deligation
+
+User::~User() {
+    cout << "User destructor called." << endl;
+}
+
+void User::getName() const {
+    cout << "User Name: " << name << endl;
+}
+
+void User::getAge() const {
+    cout << "User Age: " << age << endl;
+}
+
+void User::borrowBook(const Book& book) {
+    borrowedBooks.push_back(book);
+}
