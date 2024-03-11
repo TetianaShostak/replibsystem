920af9a3b81eb503ae7109221b66b84782bb5ec3
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-11 22:13:35 +0200

    modif.Book.cpp added

--- Book.cpp
+++ Book.cpp
@@ -5,12 +5,24 @@
 #include <iostream>
 using namespace std;
 
+int Book::totalBooks = 0; //initial static field
+void Book::printLibraryName() {
+    cout << "Library: Memory Library" << endl;
+}
+//static
 Book::Book(const string& _title, const string& _author, int _yearPublished)
         : title(_title), author(_author), yearPublished(_yearPublished) {
     cout << "Book constructor called." << endl;
+    totalBooks++;
 }
 
 Book::Book() : Book("Unknown", "Unknown", 0) {}
+
+// copy
+Book::Book(const Book& other)
+        : title(other.title), author(other.author), yearPublished(other.yearPublished) {
+    cout << "Book copy constructor called." << endl;
+}
 
 Book::~Book() {
     cout << "Book destructor called." << endl;
@@ -27,3 +39,17 @@ void Book::getAuthor() const {
 void Book::getYearPublished() const {
     cout << "Book Year Published: " << yearPublished << endl;
 }
+
+ostream& operator<<(ostream& os, const Book& book) {
+    os << "Title: " << book.title << "\n";
+    os << "Author: " << book.author << "\n";
+    return os;
+}
+
+Book& Book::operator++() {
+    ++yearPublished;
+    return *this;
+}
+
+
+
