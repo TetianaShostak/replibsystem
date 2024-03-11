7aaa4f154d5303e90faf21b17ffe936a6628ef97
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-11 22:18:50 +0200

    modif.Book.h added

--- Book.h
+++ Book.h
@@ -16,11 +16,22 @@ private:
 public:
     Book(const string& _title, const string& _author, int _yearPublished);
     Book();
+
+    // copy
+    Book(const Book& other);
+    static void printLibraryName();
+
+    static int totalBooks;
+
+
     ~Book();
     void getTitle() const;
     void getAuthor() const;
     void getYearPublished() const;
-    };
 
+    Book& operator++();
+    friend ostream& operator<<(ostream& os, const Book& book);
+
+};
 
 #endif //UNTITLED_BOOK_H
