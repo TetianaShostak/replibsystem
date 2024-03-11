1a648012dc730a7454f7af6da62724b6f8e6766b
Tanya <shostak.tetiana@chnu.edu.ua>
2024-03-11 22:40:18 +0200

    Modif.main.cpp added

--- main.cpp
+++ main.cpp
@@ -7,8 +7,17 @@ using namespace std;
 
 int main() {
     Book book1("1984", "George Orwell", 1949);
+
+    Book book2 = book1; //creating book2, which is copy of book1
+    ++book1;
+
+    //виклик стат поля
+    cout << "Total number of books: " << Book::totalBooks << endl;
+
+    Book::printLibraryName();
+
     User user1("Mary Dave", 18);
-    Librarian librarian1("Jane Sun", 19);
+    Librarian librarian1("Jane Sun", 21);
 
     book1.getTitle();
     book1.getAuthor();
@@ -19,6 +28,13 @@ int main() {
 
     librarian1.getName();
     librarian1.getAge();
+
+    //викликаю перев. дружній оператор 
+    Book book("Da Vinci Code", "Dan Brown", 2003);
+    cout << book;
+
+    // Викликаю метод з вказівником this
+    librarian1.displayInfo();
 
     return 0;
 }
