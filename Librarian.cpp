72c7766ecb92318b0452b1244b6165b0e28f9fab
Tanya <shostak.tetiana@chnu.edu.ua>
2024-04-09 12:26:04 +0300

    Add modif.Librarian.cpp(inharitance)

---
+++ Librarian.cpp
@@ -0,0 +1,34 @@
+#include "Librarian.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+Librarian::Librarian(const string& name, int age, const string& libraryName)
+        : User(name, age), libraryName(libraryName) {
+    cout << "Librarian constructor called." << std::endl;
+}
+
+Librarian::Librarian() : Librarian("Unknown", 0, "Unknown Library") {}
+
+Librarian::~Librarian() {
+    cout << "Librarian destructor called." << std::endl;
+}
+
+void Librarian::addBookToLibrary(const Book& book) {
+    libraryBooks.push_back(book);
+}
+
+void Librarian::removeBookFromLibrary(const string& bookTitle) {
+    libraryBooks.erase(std::remove_if(libraryBooks.begin(), libraryBooks.end(), [&](const Book& book) {
+        return book.getTitle() == bookTitle;
+    }), libraryBooks.end());
+}
+
+void Librarian::displayInfo() const {
+    cout << "Librarian: " << getName() << ", Age: " << getAge()
+              << ", Library Name: " << libraryName << endl;
+}
+
+string Librarian::getLibraryName() const {
+    return libraryName;
+}
