05877091ec5c829c3421ed81a3a45393066fbf2e
Tanya <shostak.tetiana@chnu.edu.ua>
2024-04-19 07:59:46 +0300

    Add modif.Teacher.h(polym.)

--- Teacher.h
+++ Teacher.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "User.h"
 #include <string>
+using namespace std;
 
-class Teacher : public User {
+
+class Teacher final : public User {
 private:
     std::string subject;
 
 public:
-    Teacher(const std::string& name, int age, const std::string& subject);
+    Teacher(const std::string& name, int age, const std::string& subject); // Change here
     ~Teacher();
 
-    void displayInfo() const;
+    void displayInfo() const override;
+    void processInformation() const override;
 };
