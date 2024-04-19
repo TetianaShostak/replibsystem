12d5b7b59b45265bb4b9bbb63cb5ce34108efe26
Tanya <shostak.tetiana@chnu.edu.ua>
2024-04-19 07:52:36 +0300

    Add modif.main.cpp(polymorphizm)

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 
 int main() {
-
     Book book1("1984", "George Orwell", 1949);
     Book book2("The Day That Taught Me to Live", "Laurent Gounelle", 2019);
 
+    //  до та після оператора присвоєння
     cout << "Before assignment:" << endl;
     cout << book2 << endl;
     book2 = book1;
@@ -21,33 +21,53 @@ int main() {
 
     cout << "Total books(static): " << Book::getTotalBooks() << endl;
 
+    // створюю об'єкти користувачів
     Student student1("Olha Anderson", 18, "Harvard University");
-    Student studentCopy = student1;
+    Student studentCopy = student1;  // Використання конструктора копіювання
     cout << "Original Student:" << endl;
     student1.displayInfo();
+    student1.processInformation();
     cout << "Copied Student:" << endl;
     studentCopy.displayInfo();
+    studentCopy.processInformation();
 
     Student student2("Ksenia Shevchenko", 21, "Harvard");
     Student student3("Alice Smith", 19, "Oxford");
     cout << "Before assignment:" << endl;
     student3.displayInfo();
+    student3.processInformation();
     student3 = student2;
     cout << "After assignment:" << endl;
     student3.displayInfo();
+    student3.processInformation();
 
     Teacher teacher1("Mia Shostak", 20, "Physics");
     teacher1.displayInfo();
+    teacher1.processInformation();
 
     Librarian librarian1("Emily Matveychuk", 21, "MEMORY Library");
     librarian1.addBookToLibrary(book1);
     librarian1.addBookToLibrary(book2);
     librarian1.displayInfo();
-
-    User user1("John Doe", 23);
-    cout << "User Name: " << user1.getName() << ", Age: " << user1.getAge() << endl;
+    librarian1.processInformation();
 
     Library library;
+    library.addUser(&student1);
+    library.addUser(&teacher1);
+    library.addUser(&librarian1);
+
+    //  вказівник на базовий клас
+    User* userPtr = &student1;
+    userPtr->displayInfo();
+    userPtr->processInformation();
+
+    User& userRef = teacher1;
+    userRef.displayInfo();
+    userRef.processInformation();  // викликаю через посилання на базовий клас
+
+    userPtr = &librarian1;
+    userPtr->displayInfo();
+    userPtr->processInformation();
 
     return 0;
 }
