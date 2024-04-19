9fdbcea81086f1bea380f887e0c0b1f1190244b7
Tanya <shostak.tetiana@chnu.edu.ua>
2024-04-19 07:58:40 +0300

    Add modif.Teacher.cpp(polym.)

--- Teacher.cpp
+++ Teacher.cpp
@@ -1,13 +1,21 @@
 #include "Teacher.h"
 #include <iostream>
 using namespace std;
+#include "Teacher.h"
+#include <iostream>
 
-Teacher::Teacher(const string& name, int age, const string& subject)
+Teacher::Teacher(const std::string& name, int age, const std::string& subject)
         : User(name, age), subject(subject) {}
 
-Teacher::~Teacher() {}
+Teacher::~Teacher() {
+    std::cout << "Teacher destructor called." << std::endl;
+}
 
 void Teacher::displayInfo() const {
-    std::cout << "Teacher Name: " << getName() << ", Age: " << getAge()
-              << ", Subject: " << subject << std::endl;
+    std::cout << "Teacher Name: " << getName() << ", Age: " << getAge() << ", Subject: " << subject << std::endl;
 }
+
+void Teacher::processInformation() const {
+    std::cout << "Analyzing academic performance data for teacher: " << getName() << std::endl;
+}
+
