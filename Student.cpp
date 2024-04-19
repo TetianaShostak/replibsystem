785c75b16e62e11dbd59220a65026949d18111ac
Tanya <shostak.tetiana@chnu.edu.ua>
2024-04-19 08:00:55 +0300

    Add modif.Student.cpp(polym.)

--- Student.cpp
+++ Student.cpp
@@ -1,27 +1,31 @@
 #include "Student.h"
 #include <iostream>
 using namespace std;;
-Student::Student(const std::string& name, int age, const std::string& university)
+
+Student::Student(const string& name, int age, const string& university)
         : User(name, age), university(university) {}
 
-Student::Student(const Student& other)
-        : User(other), university(other.university) {
-    std::cout << "Copy constructor called for Student: " << other.getName() << std::endl;
+Student::~Student() {
+    cout << "Student destructor called" << endl;
 }
-
-
-Student::~Student() {}
-
-Student& Student::operator=(const Student& other) {
-    if (this != &other) {
-        User::operator=(other);
-        university = other.university;
-        std::cout << "Assignment operator called for Student: " << other.getName() << std::endl;
-    }
-    return *this;
-}
-
 void Student::displayInfo() const {
-    cout << "Name: " << getName() << ", Age: " << getAge()
-              << ", University: " << university << std::endl;
+    cout << "Student Name: " << getName() << ", Age: " << getAge() << ", University: " << university << endl;
 }
+
+void Student::processInformation() const {
+    cout << "Processing student information for: " << getName() << endl;
+}
+
+//Student& Student::operator=(const Student& other) {
+   // if (this != &other) {
+       // User::operator=(other);
+      //  university = other.university;
+       // std::cout << "Assignment operator called for Student: " << other.getName() << std::endl;
+ //   }
+//    return *this;
+//}
+
+//void Student::displayInfo() const {
+  //  cout << "Name: " << getName() << ", Age: " << getAge()
+ //             << ", University: " << university << std::endl;
+//}
